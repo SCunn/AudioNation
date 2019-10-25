b9ce5b3cdcde19357814595308b576ed7f1ed8b7
Your Name <ec2-user@ip-172-31-47-127.eu-west-1.compute.internal>
2019-10-25 22:53:24 +0000

    2nd commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs'); //Tells the app that all pages will be rendered i
 
 app.use(express.static("views"));// allows access to views folder
 app.use(express.static("style")); // Allows access to the styles folder
+app.use(express.static("images")); // Allows access to the images folder
 
 
 // body parser to get information
@@ -16,11 +17,51 @@ var bodyParser = require("body-parser"); // call body parser module and make use
 app.use(bodyParser.urlencoded({extended:true}));
 
 
+// +++++++ JSON ++++++++++
+
+var social = require("./model/social.json"); // allow the app to access the social.json file
+
+app.get('/social', function(req, res) {
+    res.render("social",{social});
+    console.log("social page loaded, social page json data included");
+});
 // set up middleware function invoking the request and response function
 app.get('/', function(req, res) {
 res.render("index"); // renders the index page in the web browser
 console.log("Home Page Loaded"); // used to output activity in the console
 });
+
+
+app.get('/items', function(req, res) {
+    res.render("items");
+    console.log("items page loaded");
+});
+
+app.get('/additem', function(req, res) {
+   res.render("additem");
+   console.log("additem page loaded");
+});
+
+app.get('/profile', function(req, res) {
+   res.render("profile"); 
+   console.log("profile page loaded");
+});
+
+app.get('/cart', function(req, res) {
+    res.render('cart');
+    console.log("cart page loaded");
+})
+
+app.get('/checkout', function(req, res) {
+    res.render("checkout");
+    console.log("Checkout page loaded");
+});
+
+app.get('/register', function(req, res) {
+    res.render("register");
+    console.log("registration page");
+});
+
 
 
 // This code provides the server port for the application to run on
