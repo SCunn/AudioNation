951961fb3d9acdb147cf47be60604850029da886
Your Name <ec2-user@ip-172-31-47-127.eu-west-1.compute.internal>
2019-11-09 19:03:04 +0000

    shopping cart new

---
+++ model/cart.js
@@ -0,0 +1,34 @@
+module.exports = function Cart(cart) {  //create Cart constructor function, module.exports makes code available when called elsewhere
+    this.items = cart.items || {};
+    this.totalItems = cart.totalItems || 0;
+    this.totalPrice = cart.totalPrice || 0;
+
+// add new items to the shopping cart 
+    this.add = function(item, id) {
+        var cartItem = this.items[id]; 
+        // check to see if the item added to cart is already there 
+        if (!cartItem) {    // if not, create a new item
+            cartItem = this.items[id] = {item: item, quantity: 0, price: 0};
+        }
+        cartItem.quantity++;    // increase quantity by 1
+        cartItem.price = cartItem.item.price * cartItem.quantity;   // calculate the price of the item by the quantity  
+        // This section updates the total quantity and price
+        this.totalItems++;
+        this.totalPrice += cartItem.item.price;
+    };
+
+    this.remove = function(id) {
+        this.totalItems -= this.items[id].quantity;
+        this.totalPrice -= this.items[id].price;
+        delete this.items[id];
+    };
+    
+    //generate an array to output an array of product lists
+    this.getItems = function() {    
+        var arr = [];
+        for (var id in this.items) {
+            arr.push(this.items[id]);
+        }
+        return arr;
+    };
+};
