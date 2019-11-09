6570fa4c92515d29be95bed63ade16f1661e9911
Your Name <ec2-user@ip-172-31-47-127.eu-west-1.compute.internal>
2019-11-09 15:27:51 +0000

    shopping cart

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express"); // call the express module whoich is the default provided by node
 
 var app = express();    // declaration of the app 
-
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
@@ -10,6 +10,7 @@ var session = require('express-session');
 app.set('view engine', 'ejs'); //Tells the app that all pages will be rendered in the ejs syntax unless otherwise stated
 
 app.use(cookieParser());
+// express-session setup
 app.use(session({
   secret: 'secret',
   resave: false,
@@ -20,7 +21,7 @@ app.use(express.static("views"));// allows access to views folder
 app.use(express.static("style")); // Allows access to the styles folder
 app.use(express.static("images")); // Allows access to the images folder
 
-// res.locals is an object passed to hbs engine
+// cart function res.locals is an object passed to engine
 app.use(function(req, res, next) {
     res.locals.session = req.session;
     next();
@@ -46,23 +47,23 @@ app.use(fileUpload());
 
 // -----------SQL with Gearhost database-----------
 
-const db = mysql.createConnection({
-    host: 'den1.mysql3.gear.host',  //address where the mySQL database is hosted
-    user: 'audionation',
-    password: 'Pe9o4zcizy?~',
-    database: 'AUDIONATION'
-});
+// const db = mysql.createConnection({
+//     host: 'den1.mysql3.gear.host',  //address where the mySQL database is hosted
+//     user: 'audionation',
+//     password: 'Pe9o4zcizy?~',
+//     database: 'AUDIONATION'
+// });
 
-// the next section creates the connnection to the database
+// // the next section creates the connnection to the database
 
-db.connect((err) =>{
-    if(err){
-        console.log("go back and check the connection details. Something is wrong.") // console.log is used instead of crashing the app
-        // throw(err)
-    } else {
-        console.log('database connected')
-    }
-});
+// db.connect((err) =>{
+//     if(err){
+//         console.log("go back and check the connection details. Something is wrong.") // console.log is used instead of crashing the app
+//         // throw(err)
+//     } else {
+//         console.log('database connected')
+//     }
+// });
 
 // this route will create a database table
 
@@ -144,6 +145,7 @@ console.log("Home Page Loaded"); // used to output activity in the console
 
 app.get('/items', function(req,res) {
     
+    var productId = products && products[0].id;
     res.render("items",{products});
     // let sql = 'SELECT * FROM products';
     
@@ -165,33 +167,81 @@ app.get('/additem', function(req, res) {
 });
 
 
-// post request from the additem.ejs form to database table products
-app.post('/additem', function(req, res) {
+// // post request from the additem.ejs form to database table products
+// app.post('/additem', function(req, res) {
     
-    let sampleFile = req.files.sampleFile;
-    filename = sampleFile.name;
+//     let sampleFile = req.files.sampleFile;
+//     filename = sampleFile.name;
     
-    sampleFile.mv('./images/' + filename, function(err){
+//     sampleFile.mv('./images/' + filename, function(err){
         
-        if(err)
+//         if(err)
         
-        return res.status(500).send(err);
-        console.log("Image you are uploading is " + filename)
+//         return res.status(500).send(err);
+//         console.log("Image you are uploading is " + filename)
         
-    })
+//     })
    
+//   function getBigId(records, id){ //function getBigId with parameters records and id
+//       var bIg;
+      
+//       for(var i = 0; i < records.length; i++){ // loops through the item in the JSON file as long as there are records 
+//          if(!bIg || parseInt(item[i][id]) > parseInt(bIg[id])); 
+//          bIg = records[i];
+//       }
+//       return bIg;
+//   }
+// //   // Insert into table that will show Artist, Album, image, genre, condition, description, price, sale type
+// //   let sql = 'INSERT INTO products (Artist, Album, Image, Genre, Quality, Info, Price, Purpose) VALUES("'+req.body.artist+'", "'+req.body.album+'", "'+filename+'", "'+req.body.genre+'", "'+req.body.quality+'", "'+req.body.information+'", '+req.body.price+', "'+req.body.purpose+'")';
    
-//   // Insert into table that will show Artist, Album, image, genre, condition, description, price, sale type
-//   let sql = 'INSERT INTO products (Artist, Album, Image, Genre, Quality, Info, Price, Purpose) VALUES("'+req.body.artist+'", "'+req.body.album+'", "'+filename+'", "'+req.body.genre+'", "'+req.body.quality+'", "'+req.body.information+'", '+req.body.price+', "'+req.body.purpose+'")';
-   
-//   let query = db.query(sql, (err,res) => {
+// //   let query = db.query(sql, (err,res) => {
        
-//       if(err) throw err;
+// //       if(err) throw err;
        
-//       console.log(res);
+// //       console.log(res);
+// //   });
+
+//     // create a new id for the next JSON item
+//   bigUid = getBigId(item, "id"); // calls the getBigId function from above and passes in parameters 
+   
+//   var add_Id = bigUid.id + 1; // add an id number + 1 larger than the previous id number
+   
+//   // display result in the console
+//   console.log("The New ID is" + add_Id);
+   
+//   // this section accesses what the user types in the form and passes the infromation
+//   //to the JSON file as new data
+   
+//   var item_New = { 
+      
+//       id: add_Id, 
+//       artist: req.body.artist, 
+//       album: req.body.album, 
+//       image: filename,
+//       genre: req.body.genre,
+//       quality: req.body.quality,
+//       info: req.body.information,
+//       price: req.body.price,
+//       purpose: req.body.purpose
+//   }
+   
+//   fs.readFile('./data/products.json', 'utf8',  function readfileCallback(err){
+        
+//         if(err) {
+//             throw(err)
+            
+//         } else {
+            
+//             item.push(item_New); // add the new data to the JSON file
+//             json = JSON.stringify(item, null, 9); // this line structures the JSON so it is easy on the eye
+//             fs.writeFile('./data/products.json',json, 'utf8');
+            
+//         }
+        
 //   });
-    res.redirect('/profile');
-});
+   
+//     res.redirect('/profile');
+// });
 
 app.get('/edititem/:id', function(req, res) {
     
@@ -203,11 +253,11 @@ app.get('/edititem/:id', function(req, res) {
         
     //     console.log(result);
         
-        res.render('edititem', {result});
+      //  res.render('edititem', {result});
     });
     
     
-});
+//});
 
 // URL to edititem.ejs
 
@@ -216,18 +266,18 @@ app.post('/edititem/:id', function(req, res) {
     
     
      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.sampleFile;
-    filename = sampleFile.name;
+    // let sampleFile = req.files.sampleFile;
+    // filename = sampleFile.name;
     
-    //use the mv() method to place the file somewhere on the server
-    sampleFile.mv('./images/' + filename, function(err){
+    // //use the mv() method to place the file somewhere on the server
+    // sampleFile.mv('./images/' + filename, function(err){
         
-        if(err)
+    //     if(err)
         
-        return res.status(500).send(err);
-        console.log("Image you are uploading is " + filename)
-       // res.redirect('/');
-    });    
+    //     return res.status(500).send(err);
+    //     console.log("Image you are uploading is " + filename)
+    //   // res.redirect('/');
+    // });    
     
     
     // // Update the sql database table called products
@@ -241,7 +291,7 @@ app.post('/edititem/:id', function(req, res) {
         
     // });
     
-    res.redirect('/items');
+    // res.redirect('/items');
     
    
 });
@@ -250,7 +300,7 @@ app.post('/edititem/:id', function(req, res) {
 // Url to see individual product
 app.get('/item/:id', function(req,res){
     // Create a table that will show product Id, name, price, image and sporting activity
-    let sql = 'SELECT * FROM products WHERE Id = "'+req.params.id+'" ';
+    //let sql = 'SELECT * FROM products WHERE Id = "'+req.params.id+'" ';
     
     // let query = db.query(sql, (err,result) => {
         
@@ -282,7 +332,7 @@ app.get('/delete/:id', function(req,res){
         
     // });
     
-    res.redirect('/items');
+    //res.redirect('/items');
     
     
 })
@@ -311,50 +361,43 @@ app.get('/register', function(req, res) {
 
 app.get('/addtocart/:id', function(req, res, next) {
     
-    let sql = 'SELECT * FROM products WHERE Id = "'+req.params.id+'" ';
-        let query = db.query(sql, (err,result) => {
-            
-            if(err) throw err;
-            
-            console.log(result);
-            
-    });
     var productId = req.params.id;
-    var cart = new Cart(req.session.cart ? req.session.cart : {});
-    var product = sql.filter(function(item) {
+    var cart = new Cart(req.session.cart ? req.session.cart : {});  // New Cart will be created if there is a new Item, else old session is used
+    var product = products.filter(function(item) {  // filter products json and return item.id
         return item.id == productId;
     });
     cart.add(product[0], productId);
     req.session.cart = cart;
+    console.log(req.session.cart);
     res.redirect('items');
     inline();
  
 });
 
 
-app.get('/cart', function(req, res, next) {
-      if (!req.session.cart) {
-        return res.render('cart', {
-            products: null
-    });
-  }
-  var cart = new Cart(req.session.cart);
-  res.render('cart', {
-    title: 'NodeJS Shopping Cart',
-    products: cart.getItems(),
-    totalPrice: cart.totalPrice
-  });
+// app.get('/cart', function(req, res, next) {
+//       if (!req.session.cart) {
+//         return res.render('cart', {
+//             products: null
+//     });
+//   }
+//   var cart = new Cart(req.session.cart);
+//   res.render('cart', {
+//     title: 'NodeJS Shopping Cart',
+//     products: cart.getItems(),
+//     totalPrice: cart.totalPrice
+//   });
    
-});
+// });
 
-app.get('/remove/:id', function(req, res, next) {
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {});
+// app.get('/remove/:id', function(req, res, next) {
+//   var productId = req.params.id;
+//   var cart = new Cart(req.session.cart ? req.session.cart : {});
 
-  cart.remove(productId);
-  req.session.cart = cart;
-  res.redirect('/cart');
-});
+//   cart.remove(productId);
+//   req.session.cart = cart;
+//   res.redirect('/cart');
+// });
 
 
 
@@ -363,5 +406,5 @@ app.get('/remove/:id', function(req, res, next) {
 
 // This code provides the server port for the application to run on
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
-    console.log("I'll be having some of that!!");
+    console.log("App Running!!");
 });
